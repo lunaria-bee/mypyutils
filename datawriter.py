e7cd3af3347e77abff906fd30e29b60af1ba1827
lunaria-bee <luna.peck@protonmail.com>
2026-01-19 15:22:29 -0800

    Better error checking and debug info for DataWriter

--- datawriter.py
+++ datawriter.py
@@ -11,19 +11,36 @@ class DataWriter:
             path,
             ivars=(),
             dvars=(),
+            parents=False,
+            exist_ok=False,
     ):
         '''TODO'''
+        logging.debug(
+            f"\n  path={repr(path)}"
+            f"\n  ivars={repr(ivars)}"
+            f"\n  dvars={repr(dvars)}"
+            f"\n  parents={repr(parents)}"
+            f"\n  exist_ok={repr(exist_ok)}"
+        )
+
         self.path = Path(path)
         self.ivars = tuple(ivars)
         self.dvars = tuple(dvars)
 
-        if self.path.is_file():
-            logging.debug(f"{repr(self.path)} exists")
-            with open(self.path, 'r') as f:
-                self.completed = set(
-                    tuple(row[var] for var in self.ivars)
-                    for row in csv.DictReader(f)
-                )
+        if self.path.is_dir():
+            raise FileExistsError(f"{repr(self.path)} is a directory")
+
+        elif self.path.is_file():
+            if exist_ok:
+                logging.debug(f"appending to {repr(self.path)}")
+                with open(self.path, 'r') as f:
+                    self.completed = set(
+                        tuple(row[var] for var in self.ivars)
+                        for row in csv.DictReader(f)
+                    )
+            else:
+                raise FileExistsError(f"{repr(self.path)} exists")
+
         else:
             logging.debug(f"creating {repr(self.path)}")
             with open(self.path, 'w') as f:
