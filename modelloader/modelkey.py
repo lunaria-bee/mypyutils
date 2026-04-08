f706c4ab924bec48fe806e067c29158a966d90a7
lunaria-bee <luna.peck@protonmail.com>
2026-04-08 16:24:06 -0700

    Added ModelKey unit tests

--- modelloader/modelkey.py
+++ modelloader/modelkey.py
@@ -65,3 +65,65 @@ type KeyLike = Union[
     tuple[str, Union[str, None]],
 ]
 '''Type for values that can be interpreted as :class:`ModelKey`s.'''
+
+
+import unittest
+
+class TestModelKeyComparison(unittest.TestCase):
+    def test_eq(self):
+        self.check_with_function(lambda x: x)
+
+    def test_hash(self):
+        self.check_with_function(hash)
+
+    def check_with_function(self, fn):
+        k1 = ModelKey('pythia-6.9b', None)
+        k2 = ModelKey('pythia-6.9b', None)
+        self.assertEqual(fn(k1), fn(k2))
+
+        k1 = ModelKey('pythia-6.9b', 'step1')
+        k2 = ModelKey('pythia-6.9b', 'step1')
+        self.assertEqual(fn(k1), fn(k2))
+
+        k1 = ModelKey('pythia-160m', None)
+        k2 = ModelKey('pythia-6.9b', None)
+        self.assertNotEqual(fn(k1), fn(k2))
+
+        k1 = ModelKey('pythia-6.9b', 'step1')
+        k2 = ModelKey('pythia-6.9b', None)
+        self.assertNotEqual(fn(k1), fn(k2))
+
+        k1 = ModelKey('pythia-6.9b', 'step1')
+        k2 = ModelKey('pythia-6.9b', 'step2')
+        self.assertNotEqual(fn(k1), fn(k2))
+
+        k1 = ModelKey('pythia-160m', 'step1')
+        k2 = ModelKey('pythia-6.9b', 'step1')
+        self.assertNotEqual(fn(k1), fn(k2))
+
+class TestModelKeyConversion(unittest.TestCase):
+    def test_from_model_key(self):
+        self.assertEqual(
+            ModelKey.convert_from(ModelKey('EleutherAI/pythia-6.9b', None)),
+            ModelKey('EleutherAI/pythia-6.9b', None),
+        )
+        self.assertEqual(
+            ModelKey.convert_from(ModelKey('EleutherAI/pythia-6.9b', 'step1')),
+            ModelKey('EleutherAI/pythia-6.9b', 'step1'),
+        )
+
+    def test_from_str(self):
+        self.assertEqual(
+            ModelKey.convert_from('EleutherAI/pythia-6.9b'),
+            ModelKey('EleutherAI/pythia-6.9b', None),
+        )
+
+    def test_from_tuple(self):
+        self.assertEqual(
+            ModelKey.convert_from(('EleutherAI/pythia-6.9b', None)),
+            ModelKey('EleutherAI/pythia-6.9b', None),
+        )
+        self.assertEqual(
+            ModelKey.convert_from(('EleutherAI/pythia-6.9b', 'step1')),
+            ModelKey('EleutherAI/pythia-6.9b', 'step1'),
+        )
