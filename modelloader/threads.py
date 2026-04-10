911a13309338529f9a255aead7a97dcc3a9b2c03
lunaria-bee <luna.peck@protonmail.com>
2026-04-10 16:35:04 -0700

    Reworked MainThread and added unit tests

--- modelloader/threads.py
+++ modelloader/threads.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 import huggingface_hub as hfhub
 import logging
 from pathlib import Path
+import queue
 from queue import PriorityQueue
 from threading import Event, Lock, Thread
 
@@ -11,6 +12,9 @@ from util import PathOrStr
 
 
 _log = logging.getLogger(__name__) # TODO One logger for entire modellib submodule.
+
+
+MAX_BLOCK_SECS: float = 1.
 
 
 class CompletionTracker:
@@ -73,23 +77,23 @@ class ThreadData:
 
 
 class MainThread(Thread):
-    '''TODO'''
+    '''Main thread execution class.
 
-    def __init__(
-            self,
-            cache_complete: CompletionTracker,
-            stage_complete: CompletionTracker,
-            net_msgq: PriorityQueue[NetMsg],
-            disk_msgq: PriorityQueue[DiskMsg],
-            msgq: PriorityQueue[MainMsg],
-    ):
+    The main thread:
+    - Acts as an interface between model loader internals and client code.
+    - Updates the :attr:`ThreadData.cache_complete` and
+      :attr:`ThreadData.stage_complete` bookkeeping variables.
+
+    '''
+
+    def __init__(self, thread_data: ThreadData):
         super().__init__()
-        self.cache_complete: CompletionTracker = cache_complete
-        self.stage_complete: CompletionTracker = stage_complete
-        self.net_msgq: PriorityQueue[NetMsg] = net_msgq
-        self.disk_msgq: PriorityQueue[DiskMsg] = disk_msgq
-        self.msgq: PriorityQueue[MainMsg] = msgq
+        self.thread_data: ThreadData = thread_data
+        self.msgq: ModelLoaderMessager[MainMsg] = thread_data.main_msgq
+        '''Unlayered reference to local message manager, for legibility and
+        convenience.'''
         self.stage_complete_registry: dict[ModelKey, list[Event]] = dict()
+        self._exit: bool = False
 
     def run(self):
         msg_handler_map = {
@@ -100,65 +104,116 @@ class MainThread(Thread):
             ModelStageCompleteMsg: self._handle_model_stage_complete_msg,
             ModelUnstageCompleteMsg: self._handle_model_unstage_complete_msg,
         }
-        while True:
-            msg: MainMsg = self.msgq.get()
-            _log.debug(repr(msg))
-            msg_handler_map[type(msg)](msg)
+        while not self._exit:
+            try:
+                msg: MainMsg = self.msgq.get_msg(timeout=MAX_BLOCK_SECS).content
+                msg_handler_map[type(msg)](msg)
+            except queue.Empty:
+                pass
 
-    def _handle_model_cache_cmd(self, msg: ModelCacheCmd):
-        '''TODO'''
-        if not self.cache_complete.is_complete(msg.key):
-            self.net_msgq.put(
-                ModelDownloadForCachingCmd(
-                    MSG_NORMAL_PRIORITY,
-                    msg.key,
-                    None,
-                )
+    def _handle_model_cache_cmd(self, msg: ModelCacheCmd) -> None:
+        '''Handle :class:`ModelCacheCmd`.
+
+        If model is cached, do nothing. If model is not cached, send
+        :class:`ModelDownloadForCachingCmd` to :class:`NetThread`.
+
+        '''
+        if self.thread_data.cache_complete.is_complete(msg.key):
+            _log.debug(f"{msg.key} cached: do nothing")
+        else:
+            new_msg = ModelDownloadForCachingCmd(msg.cmd_id, msg.key, None)
+            self.msgq.send_msg(
+                self.thread_data.net_msgq,
+                MSG_NORMAL_PRIORITY,
+                new_msg,
             )
 
-    def _handle_model_stage_cmd(self, msg: ModelStageCmd):
-        '''TODO'''
-        if not self.stage_complete.is_complete(msg.key):
-            self.disk_msgq.put(
-                ModelCacheToStageCmd(
-                    MSG_NORMAL_PRIORITY,
-                    msg.key,
-                    None,
-                )
+    def _handle_model_stage_cmd(self, msg: ModelStageCmd) -> None:
+        '''Handle :class:`ModelStageCmd`.
+
+        If model is staged, do nothing. If model is not staged, send
+        :class:`ModelCacheToStageCmd` to :class:`DiskThread`.
+
+        '''
+        if self.thread_data.stage_complete.is_complete(msg.key):
+            _log.debug(f"{msg.key} staged: do nothing")
+        else:
+            new_msg = ModelCacheToStageCmd(msg.cmd_id, msg.key, None)
+            self.msgq.send_msg(
+                self.thread_data.disk_msgq,
+                MSG_NORMAL_PRIORITY,
+                new_msg,
             )
 
-    def _handle_model_register_for_stage_complete_cmd(self, msg: ModelRegisterForStageCompleteCmd):
-        '''TODO'''
-        # Model already staged: set event and discard.
-        if self.stage_complete.is_complete(msg.key):
+    def _handle_model_register_for_stage_complete_cmd(
+            self,
+            msg: ModelRegisterForStageCompleteCmd,
+    ) -> None:
+        '''Handle :class:`ModelRegisterForStageCompleteCmd`.
+
+        If model is staged, immediately :meth:`~threading.Event.set()`
+        :attr:`msg.event`. If model is not staged, register :attr:`msg.event` to
+        be set once :class:`MainThread` receives the relevant
+        :class:`ModelStageCompleteMsg`.
+
+        .. _msg.event: ModelRegisterForStageCompleteCmd.event
+
+        '''
+        if self.thread_data.stage_complete.is_complete(msg.key):
+            _log.debug(f"{msg.key} staged: set event and return")
             msg.event.set()
 
-        # Model not staged, has existing registry entry: append to registry
-        # entry.
         elif msg.key in self.stage_complete_registry:
+            _log.debug(
+                f"{msg.key} not staged, has entry in registry: "
+                f"append {msg.event}"
+            )
             self.stage_complete_registry[msg.key].append(msg.event)
 
-        # Model not staged, has no registry entry: create new registry entry.
         else:
+            _log.debug(
+                f"{msg.key} not staged, no entry in registry: "
+                f"insert {{{msg.key} → {msg.event}}}"
+            )
             self.stage_complete_registry[msg.key] = [msg.event]
 
-    def _handle_model_cache_complete_msg(self, msg: ModelCacheCompleteMsg):
-        '''TODO'''
-        self.cache_complete.mark_complete(msg.key)
+    def _handle_model_cache_complete_msg(self, msg: ModelCacheCompleteMsg) -> None:
+        '''Handle :class:`ModelCacheCompleteMsg`.
 
-    def _handle_model_stage_complete_msg(self, msg: ModelStageCompleteMsg):
-        '''TODO'''
-        self.stage_complete.mark_complete(msg.key)
+        Mark :attr:`msg.key <ModelCacheCompleteMsg.key>` in
+        :attr:`~ThreadData.cache_complete`.
+
+        '''
+        self.thread_data.cache_complete.mark_complete(msg.key)
+
+    def _handle_model_stage_complete_msg(self, msg: ModelStageCompleteMsg) -> None:
+        '''Handle :class:`ModelStageCompleteMsg`.
+
+        Mark :attr:`msg.key` in :attr:`~ThreadData.stage_complete`, then
+        :meth:`~threading.Event.set()` all :class:`~threading.Event`s that are
+        registered to :attr:`msg.key` in
+        :attr:`~MainThread.stage_complete_registry`.
+
+        .. _msg.key: ModelStageCompleteMsg.key
+
+        '''
+        self.thread_data.stage_complete.mark_complete(msg.key)
 
         # Notify registered events.
         if msg.key in self.stage_complete_registry:
             for event in self.stage_complete_registry[msg.key]:
+                _log.debug(f"set {event}")
                 event.set()
             del self.stage_complete_registry[msg.key]
 
-    def _handle_model_unstage_complete_msg(self, msg: ModelUnstageCompleteMsg):
-        '''TODO'''
-        self.stage_complete.unmark_complete(msg.key)
+    def _handle_model_unstage_complete_msg(self, msg: ModelUnstageCompleteMsg) -> None:
+        '''Handle :class:`ModelUnstageCompleteMsg`.
+
+        Unmark :attr:`msg.key <ModelStageCompleteMsg.key>` in
+        :attr:`~ThreadData.stage_complete`.
+
+        '''
+        self.thread_data.stage_complete.unmark_complete(msg.key)
 
 
 class NetThread(Thread):
@@ -404,6 +459,9 @@ class DiskThread(Thread):
 
 
 import unittest
+import queue
+import tempfile
+import time
 
 class TestCompletionTracker(unittest.TestCase):
     def setUp(self):
@@ -426,3 +484,251 @@ class TestCompletionTracker(unittest.TestCase):
     def test_mark_complete_does_not_flag_other_keys_as_complete(self):
         self.tracker.mark_complete(ModelKey('a', None))
         self.assertFalse(self.tracker.is_complete(ModelKey('b', None)))
+
+class TestMainThread(unittest.TestCase):
+    KEY: ModelKey = ModelKey('EleutherAI/pythia-160m', None)
+    CMD_ID: int = 0
+
+    def setUp(self) -> None:
+        self.cachedir_handle = tempfile.TemporaryDirectory(
+            prefix="modelloader_test_cache_"
+        )
+        self.stagedir_handle = tempfile.TemporaryDirectory(
+            prefix="modelloader_test_stage_"
+        )
+        self.thread_data: ThreadData = ThreadData(
+            Path(self.cachedir_handle.name),
+            Path(self.stagedir_handle.name),
+        )
+        self.main_thread: MainThread = MainThread(self.thread_data)
+        self.main_thread.start()
+
+    def tearDown(self) -> None:
+        self.main_thread._exit = True
+        self.main_thread.join()
+        self.cachedir_handle.cleanup()
+        self.stagedir_handle.cleanup()
+
+    def test_model_cache_cmd_when_not_cached(self):
+        # Send cache command.
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelCacheCmd(self.CMD_ID, self.KEY),
+        )
+        # Check net thread msgq for ModelDownloadForCachingCmd.
+        msg = self.thread_data.net_msgq.get_msg().content
+        self.assertIsInstance(msg, ModelDownloadForCachingCmd)
+        # Mock model caching process.
+        self.thread_data.net_msgq.send_msg(
+            self.thread_data.disk_msgq,
+            MSG_HIGH_PRIORITY,
+            ModelStageToCacheCmd(msg.cmd_id, msg.key, msg.files),
+        )
+        msg = self.thread_data.disk_msgq.get_msg().content
+        self.thread_data.disk_msgq.send_msg(
+            self.thread_data.main_msgq,
+            MSG_HIGH_PRIORITY,
+            ModelCacheCompleteMsg(msg.cmd_id, msg.key),
+        )
+        # Check model is marked as cached.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(
+            self.thread_data.cache_complete.is_complete(self.KEY)
+        )
+
+    def test_model_cache_cmd_when_cached(self):
+        # Mark model as cached.
+        self.thread_data.cache_complete.mark_complete(self.KEY)
+        # Send cache command.
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelCacheCmd(self.CMD_ID, self.KEY),
+        )
+        # Verify no messages in system.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(self.thread_data.net_msgq.queue.empty())
+        self.assertTrue(self.thread_data.disk_msgq.queue.empty())
+
+    def test_model_stage_cmd_when_not_staged(self):
+        # Send stage command.
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelStageCmd(self.CMD_ID, self.KEY),
+        )
+        # Check disk thread msgq for ModelCacheToStageCmd.
+        msg = self.thread_data.disk_msgq.get_msg().content
+        self.assertIsInstance(msg, ModelCacheToStageCmd)
+        # Mock model staging process for cached model.
+        self.thread_data.disk_msgq.send_msg(
+            self.thread_data.main_msgq,
+            MSG_HIGH_PRIORITY,
+            ModelStageCompleteMsg(msg.cmd_id, msg.key),
+        )
+        # Check model is marked as staged.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(
+            self.thread_data.stage_complete.is_complete(self.KEY)
+        )
+
+    def test_model_stage_cmd_when_staged(self):
+       # Mark model as staged.
+        self.thread_data.stage_complete.mark_complete(self.KEY)
+        # Send stage comand.
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelStageCmd(self.CMD_ID, self.KEY),
+        )
+        # Verify no messages in system.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(self.thread_data.net_msgq.queue.empty())
+        self.assertTrue(self.thread_data.disk_msgq.queue.empty())
+
+    def test_model_register_for_stage_complete_cmd_with_single_model_and_single_event_when_model_is_not_staged(self):
+       # Send register command.
+        event = Event()
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelRegisterForStageCompleteCmd(self.KEY, event),
+        )
+        # Check event is not set.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertFalse(event.is_set())
+        # Mock model staging.
+        self._mock_model_staging(self.CMD_ID, self.KEY)
+        # Check event is set.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(event.is_set())
+
+    def test_model_register_for_stage_complete_cmd_with_single_model_and_multiple_events_when_model_is_not_staged(self):
+        # Register events.
+        events = [Event(), Event()]
+        for e in events:
+            self.thread_data.main_msgq.put_msg_from_client(
+                MSG_NORMAL_PRIORITY,
+                ModelRegisterForStageCompleteCmd(self.KEY, e)
+            )
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        # Check events not set.
+        self.assertFalse(any(e.is_set() for e in events))
+        # Mock model staging process.
+        self._mock_model_staging(self.CMD_ID, self.KEY)
+        # Check events are set.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(all(e.is_set() for e in events))
+
+    def test_model_register_for_stage_complete_cmd_with_multiple_models_and_single_event_per_model_when_models_are_not_staged(self):
+        key1 = self.KEY
+        key2 = ModelKey(self.KEY.hf_path, 'step1')
+        cmd_id_1 = self.CMD_ID
+        cmd_id_2 = self.CMD_ID + 1
+        # Register events.
+        event1 = Event()
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelRegisterForStageCompleteCmd(key1, event1),
+        )
+        event2 = Event()
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelRegisterForStageCompleteCmd(key2, event2),
+        )
+        # Check events not set.
+        self.assertFalse(event1.is_set())
+        self.assertFalse(event2.is_set())
+        # Mock model staging for key1.
+        self._mock_model_staging(cmd_id_1, key1)
+        # Check only event1 set.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(event1.is_set())
+        self.assertFalse(event2.is_set())
+        # Mock model staging for key2.
+        self._mock_model_staging(cmd_id_2, key2)
+        # Check both events set.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(event1.is_set())
+        self.assertTrue(event2.is_set())
+
+    def test_model_register_for_stage_complete_with_multiple_models_and_multiple_events_per_model_when_models_are_not_staged(self):
+        key1 = self.KEY
+        key2 = ModelKey(self.KEY.hf_path, 'step1')
+        cmd_id_1 = self.CMD_ID
+        cmd_id_2 = self.CMD_ID + 1
+        # Register events.
+        events1 = [Event(), Event()]
+        for e in events1:
+            self.thread_data.main_msgq.put_msg_from_client(
+                MSG_NORMAL_PRIORITY,
+                ModelRegisterForStageCompleteCmd(key1, e)
+            )
+        events2 = [Event(), Event()]
+        for e in events2:
+            self.thread_data.main_msgq.put_msg_from_client(
+                MSG_NORMAL_PRIORITY,
+                ModelRegisterForStageCompleteCmd(key2, e)
+            )
+        # Check events not set.
+        self.assertFalse(any(e.is_set() for e in events1))
+        self.assertFalse(any(e.is_set() for e in events2))
+        # Mock model staging for key1.
+        self._mock_model_staging(cmd_id_1, key1)
+        # Check only events1 set.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(all(e.is_set() for e in events1))
+        self.assertFalse(any(e.is_set() for e in events2))
+        # Mock model staging for key2.
+        self._mock_model_staging(cmd_id_2, key2)
+        # Check all events set.
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        self.assertTrue(all(e.is_set() for e in events1))
+        self.assertTrue(all(e.is_set() for e in events2))
+
+    def test_model_register_for_stage_complete_cmd_with_single_model_and_single_event_when_model_is_staged(self):
+        # Mock model staging.
+        self._mock_model_staging(self.CMD_ID, self.KEY)
+        # Send register command.
+        event = Event()
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelRegisterForStageCompleteCmd(self.KEY, event),
+        )
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        # Verify no messages in system.
+        self.assertTrue(self.thread_data.net_msgq.queue.empty())
+        self.assertTrue(self.thread_data.disk_msgq.queue.empty())
+        # Check event is set.
+        self.assertTrue(event.is_set())
+
+    def test_model_register_for_stage_complete_cmd_with_single_model_and_multiple_events_when_model_is_staged(self):
+        # Mock model staging.
+        self._mock_model_staging(self.CMD_ID, self.KEY)
+        # Register events.
+        events = [Event(), Event()]
+        for e in events:
+            self.thread_data.main_msgq.put_msg_from_client(
+                MSG_NORMAL_PRIORITY,
+                ModelRegisterForStageCompleteCmd(self.KEY, e)
+            )
+        _wait_for_empty(self.thread_data.main_msgq.queue)
+        # Verify no messages in system.
+        self.assertTrue(self.thread_data.net_msgq.queue.empty())
+        self.assertTrue(self.thread_data.disk_msgq.queue.empty())
+        # Check events are set
+        self.assertTrue(all(e.is_set() for e in events))
+
+    def _mock_model_staging(self, cmd_id: int, key: ModelKey):
+        # Mock full model staging without any unit test asserts.
+        self.thread_data.main_msgq.put_msg_from_client(
+            MSG_NORMAL_PRIORITY,
+            ModelStageCmd(cmd_id, key),
+        )
+        msg = self.thread_data.disk_msgq.get_msg().content
+        self.thread_data.disk_msgq.send_msg(
+            self.thread_data.main_msgq,
+            MSG_HIGH_PRIORITY,
+            ModelStageCompleteMsg(msg.cmd_id, msg.key),
+        )
+
+
+def _wait_for_empty(queue: queue.Queue, wait_after_empty: int = 1) -> None:
+    while not queue.empty(): pass
+    time.sleep(wait_after_empty)
