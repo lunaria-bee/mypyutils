18c38c8503b567633e75ef82945468aaefc21298
lunaria-bee <luna.peck@protonmail.com>
2026-04-10 13:21:08 -0700

    Added todo comment

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
 '''Python code I often reuse.'''
 
+# TODO Logging module? Possible features:
+# -- set typical log format(s)
+# -- add TRACE log level below debug
+
 from . import config
 from . import datawriter
