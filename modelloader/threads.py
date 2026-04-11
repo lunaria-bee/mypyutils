ec566515363582b8804daacdbc2807063c304729
lunaria-bee <luna.peck@protonmail.com>
2026-04-10 19:35:30 -0700

    Reworked NetThread and added unit tests

--- modelloader/threads.py
+++ modelloader/threads.py
@@ -223,55 +223,63 @@ class MainThread(Thread):
 class NetThread(Thread):
     '''TODO'''
 
-    def __init__(
-            self,
-            cachedir: PathOrStr,
-            stagedir: PathOrStr,
-            disk_msgq: PriorityQueue[DiskMsg],
-            msgq: PriorityQueue[NetMsg],
-    ):
+    def __init__(self, thread_data: ThreadData):
         super().__init__()
-        self.cachedir: Path = Path(cachedir)
-        self.stagedir: Path = Path(stagedir)
-        self.disk_msgq: PriorityQueue[DiskMsg] = disk_msgq
-        self.msgq: PriorityQueue[NetMsg] = msgq
+        self.thread_data: ThreadData = thread_data
+        self.msgq: ModelLoaderMessager[NetMsg] = thread_data.net_msgq
+        '''Unlayered reference to local message manager, for legibility and
+        convenience.'''
+        self._exit: bool = False
 
     def run(self):
         msg_handler_map = {
             ModelDownloadForCachingCmd: self._handle_model_download_for_caching_cmd,
             ModelDownloadForStagingCmd: self._handle_model_download_for_staging_cmd,
         }
-        while True:
-            msg: NetMsg = self.msgq.get()
-            _log.debug(repr(msg))
-            msg_handler_map[type(msg)](msg)
+        while not self._exit:
+            try:
+                msg: NetMsg = self.msgq.get_msg(timeout=MAX_BLOCK_SECS).content
+                msg_handler_map[type(msg)](msg)
+            except queue.Empty:
+                pass
 
-    def _handle_model_download_for_caching_cmd(self, msg):
-        '''TODO'''
+    def _handle_model_download_for_caching_cmd(self, msg) -> None:
+        '''Handle :class:`ModelDownloadForCachingCmd`.
+
+        Download any missing or dirty files from HuggingFace to stage, then send
+        :class:`ModelStageToCacheCmd` to :class:`DiskThread`.
+
+        '''
         self._download(msg)
-        self.disk_msgq.put(
-            ModelStageToCacheCmd(
-                MSG_HIGH_PRIORITY,
-                msg.key,
-                msg.files,
-            )
+        # TODO Use `files` field to tell disk thread to only xfer dl'ed files.
+        new_msg = ModelStageToCacheCmd(msg.cmd_id, msg.key, msg.files)
+        self.msgq.send_msg(
+            self.thread_data.disk_msgq,
+            MSG_HIGH_PRIORITY,
+            new_msg,
         )
         # TODO Unstage?
 
-    def _handle_model_download_for_staging_cmd(self, msg):
-        '''TODO'''
+    def _handle_model_download_for_staging_cmd(self, msg) -> None:
+        '''Handle :class:`ModelDownloadForStagingCmd`.
+
+        Download any missing or dirty files from HuggingFace to stage, then send
+        :class:`ModelDownloadForStagingCompleteMsg` to :class:`DiskThread`.
+
+        '''
         self._download(msg)
-        self.disk_msgq.put(
-            ModelDownloadForStagingCompleteMsg(
-                MSG_HIGH_PRIORITY,
-                msg.key,
-                msg.files,
-            )
+        new_msg = ModelDownloadForStagingCompleteMsg(msg.cmd_id, msg.key, msg.files)
+        self.msgq.send_msg(
+            self.thread_data.disk_msgq,
+            MSG_HIGH_PRIORITY,
+            new_msg,
         )
 
-    def _download(self, msg):
+    def _download(self, msg) -> None:
         '''TODO'''
-        subpath = _model_subpath(msg.key)
+        # TODO Download all missing files in single request? Look at what
+        # snapshot_download() does.
+        subpath: Path = msg.key.model_subpath()
 
         if msg.files is not None:
             # Download files specified in message.
@@ -284,10 +292,11 @@ class NetThread(Thread):
                     repo_id=msg.key.hf_path,
                     repo_type='model',
                     revision=msg.key.revision,
-                    cache_dir=self.cachedir / subpath,
+                    cache_dir=self.thread_data.cachedir / subpath,
                     dry_run=True,
                 ) if f.will_download
             )
+        _log.debug(f"downloading {files_to_dl}")
 
         # Execute download.
         for filename in files_to_dl:
@@ -295,7 +304,7 @@ class NetThread(Thread):
                 repo_id=msg.key.hf_path,
                 repo_type='model',
                 revision=msg.key.revision,
-                cache_dir=self.stagedir / subpath,
+                cache_dir=self.thread_data.stagedir / subpath,
                 filename=filename,
             )
 
@@ -462,7 +471,8 @@ class DiskThread(Thread):
             ))
 
 
-import unittest
+import unittest, unittest.mock
+import collections.abc
 import queue
 import tempfile
 import time
@@ -735,6 +745,228 @@ class TestMainThread(unittest.TestCase):
         )
 
 
+_MOCK_FILENAMES: collections.abc.Sequence[str] = ('a', 'b', 'c', 'd', 'e')
+
+
+def _mock_hfhub_snapshot_download(
+        repo_id: str,
+        *,
+        revision: str | None = None,
+        cache_dir: str | Path | None = None,
+        dry_run: bool = False,
+        **_,
+) -> list[hfhub.DryRunFileInfo]:
+    if revision is None:
+        revision = 'main'
+
+    if cache_dir is None:
+        raise ValueError("Expected cache_dir to be specified")
+    else:
+        cache_dir = Path(cache_dir)
+
+    if not dry_run:
+        raise ValueError("Expected dry_run to be True")
+
+    return [
+        hfhub.DryRunFileInfo(
+            commit_hash=revision,
+            file_size=0,
+            filename=filename,
+            local_path=str(cache_dir / repo_id / revision / filename),
+            is_cached=(cache_dir / repo_id / revision / filename).is_file(),
+            will_download=not (cache_dir / repo_id / revision / filename).is_file(),
+        ) for filename in _MOCK_FILENAMES
+    ]
+
+
+def _mock_hfhub_hf_hub_download(
+        repo_id: str,
+        filename: str,
+        *,
+        revision: str | None = None,
+        cache_dir: str | Path | None = None,
+        **_,
+) -> str:
+    if revision is None:
+        revision = 'main'
+
+    if cache_dir is None:
+        raise ValueError("Expected cache_dir to be specified")
+    else:
+        cache_dir = Path(cache_dir)
+
+    path = cache_dir / repo_id / revision / filename
+    path.parent.mkdir(exist_ok=True, parents=True)
+    path.touch()
+    return str(path)
+
+
+class TestNetThread(unittest.TestCase):
+    # TODO Stub out mock HF components?
+
+    KEY: ModelKey = ModelKey('foo', 'bar')
+    CMD_ID: int = 0
+
+    def setUp(self) -> None:
+        # Mock patchers.
+        self.hfhub_snapshot_download_patcher = unittest.mock.patch(
+            'huggingface_hub.snapshot_download',
+            side_effect=_mock_hfhub_snapshot_download,
+        )
+        self.hfhub_hf_hub_download_patcher = unittest.mock.patch(
+            'huggingface_hub.hf_hub_download',
+            side_effect=_mock_hfhub_hf_hub_download,
+        )
+        self.hfhub_snapshot_download_patcher.start()
+        self.hfhub_hf_hub_download_patcher.start()
+
+        # Thread + data.
+        self.cachedir_handle: tempfile.TemporaryDirectory
+        self.stagedir_handle: tempfile.TemporaryDirectory
+        self.thread_data: ThreadData
+        self.cachedir_handle, self.stagedir_handle, self.thread_data = \
+            _make_test_common_data()
+        self.net_thread = NetThread(self.thread_data)
+        self.net_thread.start()
+
+    def tearDown(self) -> None:
+        # Thread + data.
+        self.net_thread._exit = True
+        self.net_thread.join()
+        self.cachedir_handle.cleanup()
+        self.stagedir_handle.cleanup()
+
+        # Mock patchers.
+        self.hfhub_snapshot_download_patcher.stop()
+        self.hfhub_hf_hub_download_patcher.stop()
+
+    def test_model_download_for_caching_cmd_when_no_files_cached(self):
+        # Send command.
+        msg = ModelDownloadForCachingCmd(self.CMD_ID, self.KEY, None)
+        self.thread_data.main_msgq.send_msg(
+            self.thread_data.net_msgq,
+            MSG_NORMAL_PRIORITY,
+            msg,
+        )
+        # Check disk thread msgq for ModelStageToCacheCmd.
+        msg = self.thread_data.disk_msgq.get_msg().content
+        self.assertIsInstance(msg, ModelStageToCacheCmd)
+        # Check that files exist.
+        self.assertTrue(all(
+            _full_path(self.thread_data.stagedir, self.KEY, filename).is_file()
+            for filename in _MOCK_FILENAMES
+        ))
+        # Check that hf_hub_download called expected number of times.
+        self.assertEqual(hfhub.hf_hub_download.call_count, len(_MOCK_FILENAMES))
+
+    def test_model_download_for_caching_cmd_when_some_files_cached(self):
+        cached_files = _MOCK_FILENAMES[:len(_MOCK_FILENAMES)//2]
+        uncached_files = _MOCK_FILENAMES[len(_MOCK_FILENAMES)//2:]
+        # "Cache" files.
+        for filename in cached_files:
+            path = _full_path(self.thread_data.cachedir, self.KEY, filename)
+            path.parent.mkdir(exist_ok=True, parents=True)
+            path.touch()
+        # Send command.
+        msg = ModelDownloadForCachingCmd(self.CMD_ID, self.KEY, None)
+        self.thread_data.main_msgq.send_msg(
+            self.thread_data.net_msgq,
+            MSG_NORMAL_PRIORITY,
+            msg,
+        )
+        # Check disk thread msgq for ModelStageToCacheCmd.
+        msg = self.thread_data.disk_msgq.get_msg().content
+        self.assertIsInstance(msg, ModelStageToCacheCmd)
+        # Verify that files exist.
+        self.assertTrue(all(
+            _full_path(self.thread_data.stagedir, self.KEY, filename).is_file()
+            for filename in uncached_files
+        ))
+        # Verify that hf_hub_download was called for uncached files.
+        for filename in uncached_files:
+            self.assertTrue(any(
+                call.kwargs['filename'] == filename
+                for call in hfhub.hf_hub_download.call_args_list
+            ))
+        # Verify that hf_hub_download was NOT called for cached files.
+        for filename in cached_files:
+            self.assertFalse(any(
+                call.kwargs['filename'] == filename
+                for call in hfhub.hf_hub_download.call_args_list
+            ))
+
+    def test_model_download_for_caching_cmd_when_all_files_cached(self):
+        # "Cache" files.
+        for filename in _MOCK_FILENAMES:
+            path = _full_path(self.thread_data.cachedir, self.KEY, filename)
+            path.parent.mkdir(exist_ok=True, parents=True)
+            path.touch()
+        # Send command.
+        msg = ModelDownloadForCachingCmd(self.CMD_ID, self.KEY, None)
+        self.thread_data.main_msgq.send_msg(
+            self.thread_data.net_msgq,
+            MSG_NORMAL_PRIORITY,
+            msg,
+        )
+        # Check disk thread msgq for ModelStageToCacheCmd.
+        msg = self.thread_data.disk_msgq.get_msg().content
+        self.assertIsInstance(msg, ModelStageToCacheCmd)
+        # Verify that hf_hub_download was NOT called.
+        hfhub.hf_hub_download.assert_not_called()
+
+    def test_model_download_for_staging_cmd(self):
+        filenames = _MOCK_FILENAMES[:len(_MOCK_FILENAMES)//2]
+        # Send command.
+        msg = ModelDownloadForStagingCmd(self.CMD_ID, self.KEY, filenames)
+        self.thread_data.disk_msgq.send_msg(
+            self.thread_data.net_msgq,
+            MSG_NORMAL_PRIORITY,
+            msg,
+        )
+        # Check disk thread msgq for ModelDownloadForStagingCompleteMsg.
+        msg = self.thread_data.disk_msgq.get_msg().content
+        self.assertIsInstance(msg, ModelDownloadForStagingCompleteMsg)
+        # Check that files exist.
+        self.assertTrue(all(
+            _full_path(self.thread_data.stagedir, self.KEY, filename).is_file()
+            for filename in filenames
+        ))
+        # Check that hf_hub_download called expected number of times.
+        self.assertEqual(hfhub.hf_hub_download.call_count, len(filenames))
+
+
+def _make_test_common_data() -> tuple[
+        tempfile.TemporaryDirectory,
+        tempfile.TemporaryDirectory,
+        ThreadData,
+]:
+    cachedir_handle = tempfile.TemporaryDirectory(
+        prefix="modelloader_test_cache_"
+    )
+    stagedir_handle = tempfile.TemporaryDirectory(
+        prefix="modelloader_test_stage_"
+    )
+    thread_data = ThreadData(
+        Path(cachedir_handle.name),
+        Path(stagedir_handle.name),
+    )
+    return cachedir_handle, stagedir_handle, thread_data
+
+
 def _wait_for_empty(queue: queue.Queue, wait_after_empty: int = 1) -> None:
     while not queue.empty(): pass
     time.sleep(wait_after_empty)
+
+
+def _full_path(
+        basedir: Path,
+        key: ModelKey,
+        filename: str | Path,
+) -> Path:
+    return (
+        basedir
+        / key.model_subpath()
+        / key.hf_path
+        / (key.revision or 'main')
+        / filename
+    )
