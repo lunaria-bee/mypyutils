5fd4e005129ddce65f6f5eaa5238889d832c30fb
lunaria-bee <luna.peck@protonmail.com>
2026-06-12 17:08:04 -0700

    Reworked timer to run all time functions, instead of requiring user to specify one

--- timer.py
+++ timer.py
@@ -7,21 +7,21 @@ import time
 class Timer:
     '''Class providing a context manager for measuring code execution time.'''
 
-    def __init__(self, timefn=time.process_time):
-        self.timefn = timefn
-        '''Function that will be used to measure the start and end times of the
-        managed context.'''
-
-        self.times = []
-        '''Measured time of each managed context, in order of when each context
-        ended.'''
+    def __init__(self):
+        self.process_times: list[float] = []
+        self.thread_times: list[float] = []
+        self.clock_times: list[float] = []
 
     @contextmanager
     def time(self):
         '''Context manager that will measure execution time of the managed
         context, then save that time to ``times``.'''
-        start = self.timefn()
+        process_start = time.process_time()
+        thread_start = time.thread_time()
+        clock_start = time.time()
         try:
             yield
         finally:
-            self.times.append(self.timefn() - start)
+            self.process_times.append(time.process_time() - process_start)
+            self.thread_times.append(time.thread_time() - thread_start)
+            self.clock_times.append(time.time() - clock_start)
